0e15eb3549c74f108d6f7663dd79b56bcf3f8e5c
Daniel Bourke <danielbourke@icloud.com>
2024-07-19 12:24:44 +1000

    update docs and notebooks

--- demos/food_not_food_text_classifier/app.py
+++ demos/food_not_food_text_classifier/app.py
@@ -1,11 +1,15 @@
+# 1. Import the required packages
 import torch
 import gradio as gr
 
+from typing import Dict
 from transformers import pipeline
 
-def food_not_food_classifier(text):
+# 2. Define function to use our model on given text 
+def food_not_food_classifier(text: str) -> Dict[str, float]:
     # Set up text classification pipeline
     food_not_food_classifier = pipeline(task="text-classification", 
+                                        # Because our model is on Hugging Face already, we can pass in the model name directly
                                         model="mrdbourke/learn_hf_food_not_food_text_classifier-distilbert-base-uncased", # link to model on HF Hub
                                         device="cuda" if torch.cuda.is_available() else "cpu",
                                         top_k=None) # return all possible scores (not just top-1)
@@ -20,8 +24,11 @@ def food_not_food_classifier(text):
 
     return output_dict
 
+# 3. Create a Gradio interface with details about our app
 description = """
-A text classifier to determine if a sentence is about food or not food.
+A text classifier to determine if a sentence is about food or not food. 
+
+Fine-tuned from [DistilBERT](https://huggingface.co/distilbert/distilbert-base-uncased) on a [small dataset of food and not food text](https://huggingface.co/datasets/mrdbourke/learn_hf_food_not_food_image_captions).
 
 TK - See source code:
 """
@@ -34,5 +41,6 @@ demo = gr.Interface(fn=food_not_food_classifier,
              examples=[["I whipped up a fresh batch of code, but it seems to have a syntax error."],
                        ["A delicious photo of a plate of scrambled eggs, bacon and toast."]])
 
+# 4. Launch the interface
 if __name__ == "__main__":
     demo.launch()
--- extras/todo.md
+++ extras/todo.md
@@ -1,6 +1,8 @@
+* [ ] Move todo.md into index.md for easier navigation (one file is enough)
 * [ ] Add "getting setup" file to get started locally with the required dependencies
 * [ ] Add text classification dataset creation notebook (so people can see where the data comes from)
 * [ ] Add a Hugging Face ecosystem overview (transformers, datasets, tokenizers, torch, Hugging Face Hub, Hugging Face Spaces, etc.)
+* [ ] Add a fav icon
 * [ ] Make code-only versions of notebooks? e.g. text stripped away and only code is left
 
 ## Quarto misc
